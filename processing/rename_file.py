e85c90c687f010653deec8a7b45f05fd3756f27a
Ho Hoang Phuoc <69688044+hhoangphuoc@users.noreply.github.com>
2024-05-06 12:29:46 +0200

    adding rename and preprocessing for custom input audio

--- processing/rename_file.py
+++ processing/rename_file.py
@@ -15,18 +15,26 @@ def rename_files(path):
             spk_id = ""
             description = ""
 
-            if len(filename.split('_')) == 0 and filename[0].isupper():
+            splitter = filename.split('_')
+            if len(splitter) == 3:
+                # final case after 2 times processes - merge name of splitter[1] and splitter[2]
+                new_filename = f'{splitter[0]}_{splitter[1]}{splitter[2]}' + '.' + ext
+
+            elif len(splitter) == 2 and splitter[2] == "":
                 # handling the case where the filename is written in camel case
-                # e.g. ABeepProcessing.wav
-                spk_id = filename[0]
-                description = filename[1:]
+                # e.g. ABeepProcessing_.wav to A_BeepProcessing_.wav
+                new_filename = filename[0] + '_' + filename[1:] + '.' + ext
+                
             else:
-                splitter = filename.split('_')
                 if splitter[0] == "BEST":
                     spk_id = splitter[1]
                 else:
                     spk_id = splitter[0]
                 description = "_".join(splitter[1:])
-            new_filename = f'{spk_id}_{description}' + '.' + ext
+                new_filename = f'{spk_id}_{description}' + '.' + ext
 
             os.rename(opj(path, file), opj(path, new_filename))
+
+if __name__ == '__main__':
+    path = opj(os.getcwd(), 'data', 'Phuoc', 'input')
+    rename_files(path)
